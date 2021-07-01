dc72fe22c857e0558b361e48ed64540ac9d86ea3
Kanzada <abdykadyrkanzada222@gmail.com>
2021-07-01 12:04:26 +0600

    Updates

---
+++ precache-manifest.a35caeca9679a6d16628a9fcf527594d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bd132329d31c069636ba069b85e30e8",
+    "url": "/Pomodoro/index.html"
+  },
+  {
+    "revision": "fce2e1fe709f00d08bd8",
+    "url": "/Pomodoro/static/css/main.719136a3.chunk.css"
+  },
+  {
+    "revision": "2c6b6551e1a3895ab044",
+    "url": "/Pomodoro/static/js/2.8d744b23.chunk.js"
+  },
+  {
+    "revision": "aae0757f3c3679f699f5d39bc18f0dca",
+    "url": "/Pomodoro/static/js/2.8d744b23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fce2e1fe709f00d08bd8",
+    "url": "/Pomodoro/static/js/main.2a1d32a0.chunk.js"
+  },
+  {
+    "revision": "4b03d6336e14e1358c60",
+    "url": "/Pomodoro/static/js/runtime-main.997729c2.js"
+  },
+  {
+    "revision": "525320df0b1f39bb54c3662d70cde5c6",
+    "url": "/Pomodoro/static/media/bell.525320df.flac"
+  }
+]);
